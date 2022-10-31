f3c6b221a1bc42662f9fff630341e33c2e1b319b
Saēed Guled <saeedguled@Saeeds-MacBook-Air.local>
2022-10-30 21:55:19 -0500

    Added media query and a click function that on button click scroll the page to the top

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello!');
+
+const topBtn = document.getElementById('topBtn')
+
+
+function getToTopFunction () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
